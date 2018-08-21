fff8da01c99c1805717355bcf1389a665a8fde1b
Ambidekstr <aavolosh@gmail.com>
2018-08-21 23:05:38 +0300

    test

--- src/main/java/com/anatoliivoloshyn/cinemawebapp/service/TicketService.java
+++ src/main/java/com/anatoliivoloshyn/cinemawebapp/service/TicketService.java
@@ -3,7 +3,6 @@ package com.anatoliivoloshyn.cinemawebapp.service;
 import com.anatoliivoloshyn.cinemawebapp.dao.DAOFactory;
 import com.anatoliivoloshyn.cinemawebapp.dao.interfaces.*;
 import com.anatoliivoloshyn.cinemawebapp.entity.*;
-import org.apache.log4j.Logger;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
